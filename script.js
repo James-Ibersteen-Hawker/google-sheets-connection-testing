da2b4dc0532074e9990dee398c09b26dc67e3358
James Hawker <526rserbinenko@frhsd.com>
2026-01-10 22:36:06 +0000

    I can fetch data from google sheets!! Woohoo!!

---
+++ script.js
@@ -0,0 +1,15 @@
+const ENDPOINT =
+  "https://script.google.com/macros/s/AKfycby8rg83ZMgLbj94vJBPpc2YrB5CYSpSxmdBruP1BbmtKyusm11uNBKObMTEU3TcSEaR/exec";
+
+async function loadData() {
+  const elem = document.querySelector("#output");
+  elem.setAttribute("style", "background-color: rgb(255,0,0);");
+  try {
+    const response = await fetch(ENDPOINT);
+    const data = await response.json();
+    elem.textContent = JSON.stringify(data, null, 2);
+  } catch (err) {
+    elem.textContent = `Error: ${err}`;
+  }
+  elem.setAttribute("style", "background-color: rgb(0,255,0);");
+}
