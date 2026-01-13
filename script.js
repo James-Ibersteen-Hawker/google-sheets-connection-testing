cc48d1f19393da646628bdc36822c767c1207bfe
James Hawker <526rserbinenko@frhsd.com>
2026-01-13 16:57:08 +0000

    fixed images

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const ENDPOINT =
   "https://script.google.com/macros/s/AKfycby8rg83ZMgLbj94vJBPpc2YrB5CYSpSxmdBruP1BbmtKyusm11uNBKObMTEU3TcSEaR/exec";
 
 async function loadData() {
+  console.time("load start");
   const elem = document.querySelector("#output");
   elem.setAttribute("style", "background-color: rgb(255,0,0);");
   const img = document.querySelector("#imgOutput");
@@ -10,9 +11,11 @@ async function loadData() {
   try {
     const response = await fetch(ENDPOINT);
     const data = await response.json();
+    console.log(data);
+    console.timeEnd("load start");
+    elem.setAttribute("style", "background-color: rgb(0,255,0);");
     elem.textContent = JSON.stringify(data, null, 2);
     img.src = data[0].IMAGES;
-    elem.setAttribute("style", "background-color: rgb(0,255,0);");
   } catch (err) {
     elem.textContent = `Error: ${err}`;
   }
