f1fcf1df01a1c92176ba03e66da3456a45d946ad
James Hawker <526rserbinenko@frhsd.com>
2026-01-13 00:58:19 +0000

    img separated and colored for testing

--- script.js
+++ script.js
@@ -4,13 +4,16 @@ const ENDPOINT =
 async function loadData() {
   const elem = document.querySelector("#output");
   elem.setAttribute("style", "background-color: rgb(255,0,0);");
+  const img = document.querySelector("#imgOutput");
+  elem.textContent = "output loading";
+  img.src = "";
   try {
     const response = await fetch(ENDPOINT);
     const data = await response.json();
     elem.textContent = JSON.stringify(data, null, 2);
-    elem.insertAdjacentHTML("beforeend", `<img src="${data[0].IMAGES}" />`);
+    img.src = data[0].IMAGES;
+    elem.setAttribute("style", "background-color: rgb(0,255,0);");
   } catch (err) {
     elem.textContent = `Error: ${err}`;
   }
-  elem.setAttribute("style", "background-color: rgb(0,255,0);");
 }
