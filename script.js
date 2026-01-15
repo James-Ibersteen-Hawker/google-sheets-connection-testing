f0bc39e00d3944c888590b9a9568673ee9d58472
James Hawker <526rserbinenko@frhsd.com>
2026-01-15 15:21:48 +0000

    commit for the bg settings

--- script.js
+++ script.js
@@ -13,9 +13,19 @@ async function loadData() {
     const data = await response.json();
     console.log(data);
     console.timeEnd("load start");
-    elem.setAttribute("style", "background-color: rgb(0,255,0);");
+    // elem.setAttribute("style", "background-color: rgb(0,255,0);");
     elem.textContent = JSON.stringify(data, null, 2);
-    img.src = data[0].IMAGES;
+    const { DATE, NAME, TEST, BG, IMAGE } = data[0];
+    elem.setAttribute(
+      "style",
+      `background: ${
+        (() => {
+          if (BG.includes("http")) return `url(${BG})`;
+          else return BG;
+        })() || "rgb(255,0,255)"
+      }`
+    );
+    img.src = IMAGE;
   } catch (err) {
     elem.textContent = `Error: ${err}`;
   }
