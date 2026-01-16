e5483ce57f4af6c9406c6eb1640d0038814b191c
James Hawker <526rserbinenko@frhsd.com>
2026-01-16 15:27:35 +0000

    some small improvements, I think the backend works quite well

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const ENDPOINT =
   "https://script.google.com/macros/s/AKfycby8rg83ZMgLbj94vJBPpc2YrB5CYSpSxmdBruP1BbmtKyusm11uNBKObMTEU3TcSEaR/exec";
 
 async function loadData() {
-  console.time("load start");
   const elem = document.querySelector("#output");
   elem.setAttribute("style", "background-color: rgb(255,0,0);");
   const img = document.querySelector("#imgOutput");
@@ -11,21 +10,14 @@ async function loadData() {
   try {
     const response = await fetch(ENDPOINT);
     const data = await response.json();
-    console.log(data);
-    console.timeEnd("load start");
-    // elem.setAttribute("style", "background-color: rgb(0,255,0);");
-    elem.textContent = JSON.stringify(data, null, 2);
-    const { DATE, NAME, TEST, BG, IMAGE } = data[0];
-    elem.setAttribute(
-      "style",
-      `background: ${
-        (() => {
-          if (BG.includes("http")) return `url(${BG})`;
-          else return BG;
-        })() || "rgb(255,0,255)"
-      }`
-    );
-    img.src = IMAGE;
+    elem.textContent = JSON.stringify(data);
+    const { IMAGE1, BG } = data[0];
+    const makeBG = (BG) => {
+      if (BG.includes("http")) return `url(${BG})`;
+      else return BG || "rgb(0,255,0)";
+    };
+    elem.setAttribute("style", `background: ${makeBG(BG)}`);
+    img.src = IMAGE1;
   } catch (err) {
     elem.textContent = `Error: ${err}`;
   }
